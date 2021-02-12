1d4acc96ead2867de62c2b0f94a899575f7121d6
Ethan Andrew Fulcher <fulchere@msu.edu>
2021-02-12 15:56:29 -0500

    delete hello world function

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Firestore under the path /messages/:documentId/original
 exports.addMessage = functions.https.onRequest(async (req, res) => {
