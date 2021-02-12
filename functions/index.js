6ec6c8c21bddc9df4af5ec34ab156ece22e19e9b
Ethan Andrew Fulcher <fulchere@msu.edu>
2021-02-12 16:10:58 -0500

    added add rating function

--- functions/index.js
+++ functions/index.js
@@ -8,11 +8,22 @@ admin.initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({original: original});
+exports.addRating = functions.https.onRequest(async (req, res) => {
+
+  // Grab the required parameters
+  const event_id = req.query.event_id;
+  const rating = req.query.rating;
+  const speaker_id = req.query.speaker_id;
+  const timestamp = req.query.timestamp;
+  
+  const new_rating = {event_id: event_id,
+                      rating: rating,
+                      speaker_id: speaker_id,
+                      timestamp: timestamp}
+
+  // Push the new rating into the ratings collection within Firestore
+  const writeResult = await admin.firestore().collection('ratings').add(new_rating);
+
   // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
+  res.json({result: `New rating added to ratings collection with ID: ${writeResult.id} added.`});
 });
