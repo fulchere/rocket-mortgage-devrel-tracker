516f0d02d65ff9da1671d7d58a3a64b666e8c467
benyukhi <benyukhi@msu.edu>
2021-02-13 17:27:39 -0600

    Added Events

--- functions/index.js
+++ functions/index.js
@@ -7,46 +7,6 @@ const admin = require('firebase-admin');
 const { firebaseConfig } = require('firebase-functions');
 admin.initializeApp();
 
-exports.addRating = functions.https.onRequest(async (req, res) => {
-
-  // Grab the required parameters
-  const event_id = req.query.event_id;
-  const rating = req.query.rating;
-  const speaker_id = req.query.speaker_id;
-  const timestamp = req.query.timestamp;
-  
-  const new_rating = {event_id: parseInt(event_id),
-                      rating: parseInt(rating),
-                      speaker_id: parseInt(speaker_id),
-                      timestamp: timestamp}
-
-  // Push the new rating into the ratings collection within Firestore
-  const writeResult = await admin.firestore().collection('ratings').add(new_rating);
-
-  // Send back a message that we've successfully written the message
-  res.json({result: `New rating added to ratings collection with ID: ${writeResult.id} added.`});
-});
-
-exports.addHost = functions.https.onRequest(async (req, res) => {
-
-  // Grab the required parameters
-  const email = req.query.email;
-  const event_ids = req.query.event_ids.split(',');
-  const name = req.query.name;
-  const phone_number = req.query.phone_number;
-  
-  const new_host = {email: email,
-                    event_ids: event_ids,
-                    name: name,
-                    phone_number: phone_number}
-
-  // Push the new rating into the hosts collection within Firestore
-  const writeResult = await admin.firestore().collection('hosts').add(new_host);
-
-  // Send back a message that we've successfully written the message
-  res.json({result: `New rating added to hosts collection with ID: ${writeResult.id} added.`});
-});
-
 exports.addBooth = functions.https.onRequest(async (req, res) => {
 
     // Grab the required parameters
@@ -85,4 +45,78 @@ exports.addCallForPapers = functions.https.onRequest(async (req, res) => {
 
     // Send back a message that we've successfully written the message
     res.json({result: `New rating added to callForPapers collection with ID: ${writeResult.id} added.`});
+});
+
+exports.addEvent = functions.https.onRequest(async (req, res) => {
+
+    // Grab the required parameters
+    const address = req.query.address;
+    const attendees = req.query.attendees;
+    const dei_affiliation = req.query.dei_affiliation;
+    const description = req.query.description;
+    const end = req.query.end;
+    const facility = req.query.facility;
+    const host_ids = req.query.host_ids.split(',');
+    const name = req.query.name;
+    const recruiting_partner = req.query.recruiting_partner;
+    const seasonality = req.query.seasonality;
+    const start = req.query.start;
+
+    const new_event = {address: address,
+        attendees: parseInt(attendees),
+        dei_affiliation: dei_affiliation,
+        description: description,
+        end: end,
+        facility: facility,
+        host_ids: host_ids,
+        name: name,
+        recruiting_partner: recruiting_partner,
+        seasonality: seasonality,
+        start: start}
+
+    // Push the new rating into the hosts collection within Firestore
+    const writeResult = await admin.firestore().collection('events').add(new_event);
+
+    // Send back a message that we've successfully written the message
+    res.json({result: `New rating added to events collection with ID: ${writeResult.id} added.`});
+});
+
+exports.addHost = functions.https.onRequest(async (req, res) => {
+
+    // Grab the required parameters
+    const email = req.query.email;
+    const event_ids = req.query.event_ids.split(',');
+    const name = req.query.name;
+    const phone_number = req.query.phone_number;
+
+    const new_host = {email: email,
+        event_ids: event_ids,
+        name: name,
+        phone_number: phone_number}
+
+    // Push the new rating into the hosts collection within Firestore
+    const writeResult = await admin.firestore().collection('hosts').add(new_host);
+
+    // Send back a message that we've successfully written the message
+    res.json({result: `New rating added to hosts collection with ID: ${writeResult.id} added.`});
+});
+
+exports.addRating = functions.https.onRequest(async (req, res) => {
+
+  // Grab the required parameters
+  const event_id = req.query.event_id;
+  const rating = req.query.rating;
+  const speaker_id = req.query.speaker_id;
+  const timestamp = req.query.timestamp;
+  
+  const new_rating = {event_id: parseInt(event_id),
+                      rating: parseInt(rating),
+                      speaker_id: parseInt(speaker_id),
+                      timestamp: timestamp}
+
+  // Push the new rating into the ratings collection within Firestore
+  const writeResult = await admin.firestore().collection('ratings').add(new_rating);
+
+  // Send back a message that we've successfully written the message
+  res.json({result: `New rating added to ratings collection with ID: ${writeResult.id} added.`});
 });
