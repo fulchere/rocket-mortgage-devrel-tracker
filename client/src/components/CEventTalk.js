fc9f4f89628797c334486b49cd15c34c740651f9
wangsh88 <78180997+wangsh88@users.noreply.github.com>
2021-02-18 18:02:28 -0500

    add some component to talk page

--- client/src/components/CEventTalk.js
+++ client/src/components/CEventTalk.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Container, Row, Col, ListGroup, ListGroupItem,} from 'shards-react'
+import DatePicker from "react-datepicker";
 
 export default function CEventTalk() {
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Row><Col><h2>Example Talk</h2></Col></Row>
+            <Row>
+                <Col>
+                    <Row>
+                        <Container>
+                        <Row>
+                        <div>Example Description</div>
+                        </Row>
+                        </Container>
+                    </Row>
+                </Col>
+                <Col>
+                <Row>Example Talk given at</Row>
+                    <Row>
+                    <Container>
+                        <ListGroup>
+                        <ListGroupItem>Example Event 1</ListGroupItem>
+                        <ListGroupItem>Example Event 2</ListGroupItem>
+                        <ListGroupItem>Example Event 3</ListGroupItem>
+                        <ListGroupItem>Example Event 4</ListGroupItem>
+                        </ListGroup>
+                        </Container>
+                    </Row>
+                </Col>
+
+            </Row>
+        </Container>
     )
 }
