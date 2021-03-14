13027618e5a0e3521269fe6277ada46fa08e3967
wangsh88 <78180997+wangsh88@users.noreply.github.com>
2021-03-14 00:30:58 -0500

    media page

--- client/src/components/CEvent.js
+++ client/src/components/CEvent.js
@@ -4,29 +4,29 @@ import CEventConference from './CEventConference'
 import CEventTalk from './CEventTalk'
 import CEventMedia from './CEventMedia'
 
-export default function CEvent({type, event_id}) {
+export default function CEvent({ type, event_id }) {
 
-    if (type === 'Conference'){
+    if (type === 'Conference') {
         return (
-            <div style={{margin:'20px'}}>
-            <CEventConference event_id={event_id}/>
+            <div style={ { margin: '20px' } }>
+                <CEventConference event_id={ event_id } />
             </div>
         )
     }
-    else if (type === 'Talk'){
+    else if (type === 'Talk') {
         return (
-            <div style={{margin:'20px'}}>
-            <CEventTalk event_id={event_id}/>
+            <div style={ { margin: '20px' } }>
+                <CEventTalk event_id={ event_id } />
             </div>
         )
     }
-    else if (type === 'Media'){
-        <div style={{margin:'20px'}}>
-        <CEventMedia event_id={event_id}/>
+    else if (type === 'Media') {
+        return <div style={ { margin: '20px' } }>
+            <CEventMedia event_id={ event_id } />
         </div>
     }
-    
-    return(
+
+    return (
         <div></div>
     )
 }
