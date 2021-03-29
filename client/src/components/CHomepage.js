a39be68c7320bfd742a820e122203607e182b6fb
Trent Belanger <59448025+belan1@users.noreply.github.com>
2021-03-29 04:04:33 -0400

    reverted deletions

--- client/src/components/CHomepage.js
+++ client/src/components/CHomepage.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
 
+import { ListGroup, ListGroupItem, Button, Container, Row, Col, Modal, ModalBody, FormInput,FormTextarea    } from "shards-react";
+
+
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import CAPIService from './CAPIService'
+import CHomepageFeed from './CHomepageFeed'
 
 import { useAuth } from '../contexts/AuthContext'
 
@@ -13,18 +17,10 @@ export default function CHomepage() {
 
     const localizer = momentLocalizer(moment);
 
-    const [event_data, setEvent_data] = useState([
-        {
-          start: moment().toDate(),
-          end: moment()
-            .add(1, "days")
-            .toDate(),
-          title: "Some title"
-        }
-      ])
+    const [event_data, setEvent_data] = useState([])
 
 
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     const { currentUser } = useAuth()
 
@@ -39,10 +35,13 @@ export default function CHomepage() {
             var temp_array = []
             for (var i = 0; i < response.event_pairs.length; i++){
               temp_array.push({
-                name : response.event_pairs[i].event_name,
-                id : response.event_pairs[i].id      
+                title : response.event_pairs[i].event_name,
+                id : response.event_pairs[i].id,
+                start : moment(response.event_pairs[i].event_start_time).toDate(),
+                end : moment(response.event_pairs[i].event_end_time).toDate()    
               })
             }
+            setEvent_data(temp_array)
           })
     
           return function cleanup() {
@@ -55,8 +54,17 @@ export default function CHomepage() {
 
     return (
         <div> {loading ? <p>loading...</p> :
-            <div>       
-                <Calendar localizer={localizer} defaultDate={new Date()} defaultView="month" events={event_data} style={{ height: "100vh" }} />
+          <div>
+            <Container  style = {{paddingTop: "20px", maxWidth: "90%"}}>    
+              <Row>
+            <Col sm="12" md="4" lg="3">
+            <CHomepageFeed event_data={event_data}/>
+            </Col>
+            <Col>
+                <Calendar localizer={localizer} defaultDate={new Date()} defaultView="month" events={event_data} style={{ height: "100vh" }} />  
+                </Col>
+                </Row>
+            </Container>
             </div>
             }
             </div>
